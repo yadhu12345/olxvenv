f414071d71174b60070c6bac511b39142eeb2762
YADHU KRISHNA P V <116164765+yadhu12345@users.noreply.github.com>
2023-04-25 21:15:37 +0530

    09:15

--- olxscrap/olxscrap/spiders/product.py
+++ olxscrap/olxscrap/spiders/product.py
@@ -1,12 +1,18 @@
 import scrapy
 
+
 class olx(scrapy.Spider):
     name='product'
     start_urls=['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723']
+    def parser(self,response):
+        prod=response.css('main._1oFYt qmRfv')
+        yield{
+            'price':prod.css('span._2Ks63::text').get(),
+        }
     def parse(self,response):
         for products in response.css('li._1DNjI'):
-            yield {
-                'name':products.css('span._2poNJ::text').get(),
-                'price':products.css('span._2Ks63::text').get(),
-                'link':products.css('a').attrib['href']
-                }
+            next_page = products.css('a').attrib['href']
+            if next_page is not None:
+                yield response.follow(next_page)
+    
+        
