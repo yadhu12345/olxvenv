e2c1d6dbc77f991de016f053aae8af9716852442
YADHU KRISHNA P V <116164765+yadhu12345@users.noreply.github.com>
2023-04-26 11:54:59 +0530

    bedrooms

--- olxscrap/olxscrap/spiders/product.py
+++ olxscrap/olxscrap/spiders/product.py
@@ -13,15 +13,15 @@ class olx(scrapy.Spider):
     def details(self,response):
         prod=response.css('main._1oFYt.qmRfv')
         yield{
-            'price':prod.css('span.T8y-z::text').get(),
             'property_name':prod.css('span.dBLgK::text').get(),
-            'property_id':prod.css('strong::text').get(),
-            'description':prod.css('p::text').get(),
+            'property_id':prod.css('strong::text')[2].get(),
             'breadcrumbs':prod.css('a._26_tZ::text').getall(),
+            'price':prod.css('span.T8y-z::text').extract(),
             'image_url':prod.css('img._1Iq92::attr(src)').get(),
+            'description':prod.css('p::text').get(),
             'seller_name':prod.css('div.eHFQs::text').get(),
             'location':prod.css('span._1RkZP::text').get(),
-            'property_type':prod.css('span.B6X7c:text').get(),
-            'bathrooms':prod.css('span.B6X7c::text').get(),
-            'bedrooms':prod.css('span.B6X7c::text').get()
+            'property_type':prod.css('span.B6X7c::text').get(),
+            'bathrooms':prod.css('span.B6X7c::text')[2].get(),
+            'bedrooms':prod.css('span.B6X7c::text')[1].get()
         }    
